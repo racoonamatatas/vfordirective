28382d28694b83c1bc446bc437c47e2c3f509e39
racoonamatatas <emacanghaill@gmail.com>
2026-01-07 12:21:31 +0100

    Render reactive tasks array as list using v-for

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
-<template></template>
+<script setup>
+import {ref} from 'vue';
 
-<script setup></script>
+const header = ref('To Do List');
+
+// Reactive array with objects
+const tasks = ref([
+    {name: 'Do the messages', completed: false},
+    {name: 'Washing up', completed: true},
+    {name: 'Walk the dog', completed: false},
+]);
+</script>
+
+<template>
+    <h1>{{ header }}</h1>
+    <ul>
+        <li v-for="task in tasks" :key="task.name">
+            {{ task.name }} {{ task.completed ? '(Completed)' : '(Not Completed)' }}
+        </li>
+    </ul>
+</template>
